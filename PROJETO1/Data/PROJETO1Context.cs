63be9b3f4815407ba1869ebbb42169ba9cae8bf3
jacques moraes <jacquesbmoraes@hotmail.com>
2021-01-09 20:37:07 -0300

    crud scaffolfing

---
+++ PROJETO1/Data/PROJETO1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PROJETO1.Models;
+
+namespace PROJETO1.Data
+{
+    public class PROJETO1Context : DbContext
+    {
+        public PROJETO1Context (DbContextOptions<PROJETO1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PROJETO1.Models.Department> Department { get; set; }
+    }
+}
--- PROJETO1/Startup.cs
+++ PROJETO1/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using PROJETO1.Data;
 
 namespace PROJETO1
 {
@@ -33,6 +35,9 @@ namespace PROJETO1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<PROJETO1Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PROJETO1Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
