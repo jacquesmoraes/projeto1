fcebceb898091eec189c83bd2ee8b2c21d0e9f32
jacques moraes <jacquesbmoraes@hotmail.com>
2021-01-09 21:23:21 -0300

    MySQL adaptation and first migration

--- PROJETO1/Startup.cs
+++ PROJETO1/Startup.cs
@@ -37,7 +37,8 @@ namespace PROJETO1
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<PROJETO1Context>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("PROJETO1Context")));
+                    options.UseMySql(Configuration.GetConnectionString("PROJETO1Context"), builder => 
+                    builder.MigrationsAssembly("PROJETO1")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
