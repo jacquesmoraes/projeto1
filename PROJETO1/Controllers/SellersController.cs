42dcca59f53b0662c4409972425f47e0f0df2688
jacques moraes <jacquesbmoraes@hotmail.com>
2021-01-10 15:16:10 -0300

    Simple Create form

--- PROJETO1/Controllers/SellersController.cs
+++ PROJETO1/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PROJETO1.Models;
 using PROJETO1.Services;
 
 namespace PROJETO1.Controllers
@@ -20,6 +21,17 @@ namespace PROJETO1.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
