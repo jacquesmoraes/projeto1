7fb48ed53a42f6f2acde083e818f1bb2b20f1e48
jacques moraes <jacquesbmoraes@hotmail.com>
2021-01-10 01:16:15 -0300

    Other entities and second migration

--- PROJETO1/Data/PROJETO1Context.cs
+++ PROJETO1/Data/PROJETO1Context.cs
@@ -14,6 +14,8 @@ namespace PROJETO1.Data
         {
         }
 
-        public DbSet<PROJETO1.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecords { get; set; }
     }
 }
